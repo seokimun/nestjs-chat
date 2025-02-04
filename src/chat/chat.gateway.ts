3f6f57976ef887051bb548bae6c0ce38d6b40872
seokimun <gimun08@naver.com>
2025-02-04 20:25:47 +0900

    ✨

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,19 +1,43 @@
+import { Logger } from '@nestjs/common';
 import {
     ConnectedSocket,
     MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    OnGatewayInit,
     SubscribeMessage,
     WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatGateway
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+    private logger = new Logger('chat');
+
+    constructor() {
+        this.logger.log('constructor');
+    }
+
+    afterInit() {
+        this.logger.log('init');
+    }
+
+    handleConnection(@ConnectedSocket() socket: Socket) {
+        this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
+    }
+
+    handleDisconnect(@ConnectedSocket() socket: Socket) {
+        this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+    }
+
     @SubscribeMessage('new_user')
     handleNewUser(
         @MessageBody() username: string,
         @ConnectedSocket() socket: Socket,
     ) {
-        console.log(socket.id);
-        console.log(username);
+        socket.broadcast.emit('user_connected', username);
+        return username;
     }
 }
