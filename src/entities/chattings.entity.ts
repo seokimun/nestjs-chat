e87febac326f1f634a5eacbd0559f8ce56c5e6be
seokimun <gimun08@naver.com>
2025-02-05 17:21:54 +0900

    feat: 🔧 테이블 생성 - 소켓 테이블 생성 - 채팅 테이블 생성

---
+++ src/entities/chattings.entity.ts
@@ -0,0 +1,28 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { SocketEntity } from './sockets.entity';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+@Entity('chattings')
+export class ChattingEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => SocketEntity, (socket) => socket.chatting, { eager: true })
+    @JoinColumn({ name: 'user_id' })
+    user: SocketEntity;
+
+    @Column({ type: 'text', nullable: false })
+    @IsNotEmpty()
+    @IsString()
+    chat: string;
+
+    @CreateDateColumn({ type: 'datetime' })
+    created_at: Date;
+}
