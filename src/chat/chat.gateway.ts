1d5879823b010acb52400a6abd4bf436616cda64
seokimun <gimun08@naver.com>
2025-02-06 21:38:34 +0900

    feat: ✨ 서버와 데이터베이스 연동 및 기능 구현

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import {
     ConnectedSocket,
     MessageBody,
@@ -9,6 +10,9 @@ import {
     WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { ChattingEntity } from '../entities/chattings.entity';
+import { Repository } from 'typeorm';
+import { SocketEntity } from '../entities/sockets.entity';
 
 @WebSocketGateway({ namespace: 'chattings' })
 export class ChatGateway
@@ -16,7 +20,12 @@ export class ChatGateway
 {
     private logger = new Logger('chat');
 
-    constructor() {
+    constructor(
+        @InjectRepository(ChattingEntity)
+        private readonly chattingRepository: Repository<ChattingEntity>,
+        @InjectRepository(SocketEntity)
+        private readonly socketRepository: Repository<SocketEntity>,
+    ) {
         this.logger.log('constructor');
     }
 
@@ -28,27 +37,60 @@ export class ChatGateway
         this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
     }
 
-    handleDisconnect(@ConnectedSocket() socket: Socket) {
+    async handleDisconnect(@ConnectedSocket() socket: Socket) {
+        const user = await this.socketRepository.findOne({
+            where: { id: socket.id },
+        });
+        if (user) {
+            socket.broadcast.emit('disconnect_user', user.username);
+            await this.socketRepository.delete(user.id);
+        }
         this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
     }
 
     @SubscribeMessage('new_user')
-    handleNewUser(
+    async handleNewUser(
         @MessageBody() username: string,
         @ConnectedSocket() socket: Socket,
     ) {
+        const exist = await this.socketRepository.findOne({
+            where: { username },
+        });
+
+        if (exist) {
+            username = `${username}_${Math.floor(Math.random() * 100)}`;
+        }
+
+        const user = this.socketRepository.create({
+            id: socket.id,
+            username,
+        });
+
+        await this.socketRepository.save(user);
+
         socket.broadcast.emit('user_connected', username);
         return username;
     }
 
     @SubscribeMessage('submit_chat')
-    handleSubmitChat(
+    async handleSubmitChat(
         @MessageBody() chat: string,
         @ConnectedSocket() socket: Socket,
     ) {
+        const socketObj = await this.socketRepository.findOne({
+            where: { id: socket.id },
+        });
+
+        const chats = this.chattingRepository.create({
+            user: socketObj,
+            chat: chat,
+        });
+
+        await this.chattingRepository.save(chats);
+
         socket.broadcast.emit('new_chat', {
             chat,
-            username: socket.id,
+            username: socketObj.username,
         });
     }
 }
--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocketEntity } from '../entities/sockets.entity';
+import { ChattingEntity } from '../entities/chattings.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([SocketEntity, ChattingEntity])],
     providers: [ChatGateway],
 })
 export class ChatModule {}
